ba782fdf8f844cef8dacd63cb137c52c0fa13c04
Kathryn Reeve <Kathryn@binaryKitten.com>
2014-07-16 23:32:47 +0100

    removed fixed footer on front page - fixes #2

---
+++ htdocs/wp-content/themes/hacmelany/functions.php
@@ -0,0 +1,29 @@
+<?php
+
+if (!function_exists('melany_footer_class')) :
+
+    /**
+     * Print CSS classes for site footer
+     *
+     * @since 1.0.0
+     *
+     * @param string $custom Optional. Custom CSS classes
+     */
+    function melany_footer_class($custom = '')
+    {
+        $class = 'site-footer navbar';
+        $color = get_theme_mod('melany_navbar_color');
+        if ($color == '' || empty($color)) {
+            $color = 'inverse';
+        }
+
+        $class .= ' navbar-' . $color;
+
+        if (!empty($custom)) {
+            $class .= ' ' . $custom;
+        }
+
+        echo 'class="' . $class . '"';
+    }
+
+endif;
